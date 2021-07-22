850a8bc0570a4ecc4071cec221da78480172fa0d
Blank <745165806@qq.com>
2021-07-22 03:20:24 +0000

    fix

--- speechtask/model/lstm.py
+++ speechtask/model/lstm.py
@@ -89,3 +89,4 @@ class UpdateModel(paddle.callbacks.Callback):
     # This callback reset model hidden states and update learning rate before each epoch begins 
     def on_epoch_begin(self, epoch=None, logs=None):
         self.model.network.reset_states()
+        
